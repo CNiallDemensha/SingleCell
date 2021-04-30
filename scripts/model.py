c2a239fa88ea7a0b79aca693c253e1b5ef6cf42d
Vishal <bvishal.athreya@gmail.com>
2021-04-30 06:39:03 -0400

    Leo code for VGMMM

--- scripts/model.py
+++ scripts/model.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 import torch.distributions as D
 import numpy as np
 from sklearn.cluster import KMeans
 import time
+import pdb
+import torch.nn.functional as F
 
 class Timer(object):
     def __init__(self, name='Operation'):
@@ -14,21 +17,222 @@ class Timer(object):
 
     def __exit__(self, type, value, traceback):
         print('%s took: %s sec' % (self.name, time.time() - self.tstart))
+class FB_GMM(nn.Module):
+    ''' A GMM model. '''
+    def __init__(
+            self, k, d, tau, mu=None):
+        super().__init__()
+        self.k = k
+        self.d = d
+        self.A = nn.Parameter(0.01 * torch.randn(self.k, self.d, self.d), requires_grad=True)
+        self.alpha = nn.Parameter(0.01 * torch.randn(self.k, self.d), requires_grad=True)
+        self.D = nn.Parameter(torch.ones(self.k, self.d), requires_grad=True)
+        self.f_mu = nn.Parameter(torch.zeros(self.k, self.d), requires_grad=True)
+        self.b_mu = nn.Parameter(torch.zeros(1, self.d), requires_grad=True)
+        self.pi = nn.Parameter(0.01 * torch.randn(self.k), requires_grad=True)
+        self.tau = tau
+    def negative_log_prob(self, x):
+        #sigma = torch.diag_embed(torch.nn.functional.softplus(self.D)) #+ torch.matmul(self.A, self.A.transpose(1,2))
+        #sigma = sigma.unsqueeze(0).repeat(x.shape[0], 1, 1, 1) # [bs, k, d, d]
+        f_mu = self.f_mu.unsqueeze(0).repeat(x.shape[0], 1, 1) # [bs, k, d]
+        b_mu = self.b_mu.unsqueeze(0).repeat(x.shape[0], 1, 1) # [bs, 1, d]
+        f_comp = D.Normal(f_mu, 1.0)
+        b_comp = D.Normal(b_mu, 1.0)
+        x = x.unsqueeze(1).repeat(1, self.k, 1) # [bs, k, d]
+        alpha = torch.sigmoid(5*self.alpha)
+        #alpha = self.alpha
+        mix_p = torch.log(torch.softmax(self.tau * self.pi, 0).unsqueeze(0)) +  \
+                torch.sum(alpha.unsqueeze(0) * f_comp.log_prob(x), -1) + \
+                torch.sum((1 - alpha.unsqueeze(0)) * b_comp.log_prob(x), -1)
+        assignment = torch.argmax(mix_p, 1)
+        nll = - torch.logsumexp(mix_p, dim=1)
+        return nll.mean(), assignment
+    def kmeans_clustering(self, samples, get_centers=True):
+        N, d = samples.shape
+        K = self.k
+        # Select random d_used coordinates out of d
+        #d_used = min(d, max(500, d//8))
+        #d_indices = np.random.choice(d, d_used, replace=False)
+        print('Performing k-means clustering to {} components of {} samples  ...'.format(K, N))
+        with Timer('K-means'):
+            clusters = KMeans(n_clusters=K, max_iter=300, n_jobs=-1).fit(samples)
+        labels = clusters.labels_
+        if get_centers:
+            centers = np.zeros([K, d])
+            for i in range(K):
+                centers[i, :] = np.mean(samples[labels == i, :], axis=0)
+            #self.mu = nn.Parameter(torch.Tensor(centers).to(self.mu.device), requires_grad=True)
+            #pdb.set_trace()
+            return labels, centers
+        return labels
+    def check_assignment(self, x):
+        sigma = torch.diag_embed(torch.nn.functional.softplus(self.D)) #+ torch.matmul(self.A, self.A.transpose(1,2))
+        sigma = sigma.unsqueeze(0).repeat(x.shape[0], 1, 1, 1) # [bs, k, d, d]
+        mu = self.mu.unsqueeze(0).repeat(x.shape[0], 1, 1) # [bs, k, d]
+        comp = D.MultivariateNormal(mu, sigma)
+        x = x.unsqueeze(1).repeat(1, self.k, 1) # [bs, k, d]
+        labels = torch.argmax(comp.log_prob(x), 1)
+        return labels.detach().cpu().numpy()
+
+class Variational_GMM_beta(nn.Module):
+    def __init__(self, k, d, hidden_dims=128, num_layers=2):
+        super().__init__()
+        self.k = k
+        self.d = d
+        self.D = nn.Parameter(torch.ones(self.k, self.d), requires_grad=True)
+        self.f_mu = nn.Parameter(torch.zeros(self.k, self.d), requires_grad=True)
+        self.b_mu = nn.Parameter(torch.zeros(1, self.d), requires_grad=True)
+        #self.prior_pi = nn.Parameter(0.1 * torch.randn(1, self.k), requires_grad=True)
+        self.prior_pi = nn.Parameter(torch.ones(1, self.k) / self.k, requires_grad=True)
+        self.q_net = [nn.Linear(d, hidden_dims), nn.ELU()]
+        for i in range(num_layers):
+            self.q_net += [nn.Linear(hidden_dims, hidden_dims), nn.ELU()]
+        self.q_net += [nn.Linear(hidden_dims, k)]
+        self.q_net = nn.Sequential(*self.q_net)
+        self.r_net = [nn.Linear(d+k, hidden_dims), nn.ELU()]
+        for i in range(num_layers):
+            self.r_net += [nn.Linear(hidden_dims, hidden_dims), nn.ELU()]
+        self.r_net += [nn.Linear(hidden_dims, d)]
+        self.r_net = nn.Sequential(*self.r_net)
+        self.beta_prior_alpha = nn.Parameter(0.01 * torch.randn(k, d), requires_grad=True)
+        self.beta_prior_beta = nn.Parameter(0.01 * torch.randn(k, d), requires_grad=True)
+        
+        
+    
+    def categorical_kl_divergence(self, p_logits, q_logits):
+        p_softmax = torch.softmax(p_logits, -1)
+        kl = torch.sum(p_softmax * F.log_softmax(p_logits, dim = 1) - p_softmax * F.log_softmax(q_logits, dim = 1), -1)
+        return kl
+    
+    def bernoulli_kl_divergence(self, p_prob, q_prob, eps=1e-4):
+        # q_prob has shape [B, k ,d]
+        # p_prob has shape [B, k, d]
+        kl = p_prob * (torch.log(eps + p_prob) - torch.log(eps + q_prob)) + (1 - p_prob) * (torch.log(eps + 1 - p_prob) - torch.log(eps + 1 - q_prob))
+        return torch.sum(kl, -1)
+    
+    def bernoulli_sample(self, p, T, eps=1e-4):
+        noise = torch.rand_like(p)
+        return torch.sigmoid((torch.log(eps + noise) - torch.log(eps + 1 - noise) + torch.log(eps + p) - torch.log(eps + 1 - p)) / T)
+
+    def elbo(self, inputs, T): # inputs [B, d]
+        z_logits = self.q_net(inputs) # [B, k]
+        z_prob = torch.softmax(z_logits, -1) # [B, K]
+        pred = torch.argmax(z_prob, -1)
+        alpha_posterior_logits = self.r_net(torch.cat([inputs, z_logits], -1)) # [B, d]
+        alpha_posterior_prob = torch.sigmoid(alpha_posterior_logits)
+        f_mu = self.f_mu.unsqueeze(0).repeat(inputs.shape[0], 1, 1) # [B, k, d]
+        b_mu = self.b_mu.unsqueeze(0).repeat(inputs.shape[0], 1, 1) # [B, 1, d]
+        f_comp = D.Normal(f_mu, 1.0)
+        b_comp = D.Normal(b_mu, 1.0)
+        inputs_tiled = inputs.unsqueeze(1).repeat(1, self.k, 1) # [B, k, d]
+        logp_f = f_comp.log_prob(inputs_tiled) #[B, k, d]
+        logp_b = b_comp.log_prob(inputs.unsqueeze(1)) #[B, 1, d]
+        E_log_p_x_given_alpha_z = torch.sum(logp_f * alpha_posterior_prob.unsqueeze(1), -1) + \
+                                  torch.sum(logp_b * (1 - alpha_posterior_prob.unsqueeze(1)), -1) # [B, k]
+        beta_prior_D = D.beta.Beta(torch.sigmoid(5*self.beta_prior_alpha.unsqueeze(0)), torch.sigmoid(5*self.beta_prior_beta.unsqueeze(0)))
+        beta = beta_prior_D.rsample()
+        kl_alpha_beta = self.bernoulli_kl_divergence(alpha_posterior_prob.unsqueeze(1), beta) # [B, k, d]
+        kl_alpha_beta_z = z_prob * kl_alpha_beta
+        E_z = torch.sum(z_prob * E_log_p_x_given_alpha_z - kl_alpha_beta_z, -1) # [B]
+        kl_q_pi = self.categorical_kl_divergence(z_logits, self.prior_pi) # [b]
+        beta_prior_D = D.beta.Beta(torch.sigmoid(5*self.beta_prior_alpha), torch.sigmoid(5*self.beta_prior_beta))
+        PI_D = D.beta.Beta(0.5*torch.ones(self.k, self.d).to(inputs.device), 0.5*torch.ones(self.k, self.d).to(inputs.device))
+        #D_beta_prior = D.Normal(torch.sigmoid(5 * self.beta_prior), 0.1*torch.ones(self.k,self.d).to(inputs.device))
+        #D_PI = D.Normal(torch.zeros(self.k, self.d).to(inputs.device), 0.1*torch.ones(self.k,self.d).to(inputs.device))
+        kl_Q_PI = D.kl.kl_divergence(beta_prior_D, PI_D).sum()
+        return torch.mean(E_z - kl_q_pi) - kl_Q_PI, torch.mean(E_z), torch.mean(kl_alpha_beta_z), torch.mean(kl_q_pi), kl_Q_PI, pred, alpha_posterior_prob, z_prob
+    
+    def forward(self, inputs, T):
+        return self.elbo(inputs.float(), T)
+
+class Variational_GMM(nn.Module):
+    def __init__(self, k, d, hidden_dims=128, num_layers=2):
+        super().__init__()
+        self.k = k
+        self.d = d
+        self.D = nn.Parameter(torch.ones(self.k, self.d), requires_grad=True)
+        self.f_mu = nn.Parameter(torch.zeros(self.k, self.d), requires_grad=True)
+        self.b_mu = nn.Parameter(torch.zeros(1, self.d), requires_grad=True)
+        #self.prior_pi = nn.Parameter(0.1 * torch.randn(1, self.k), requires_grad=True)
+        self.prior_pi = nn.Parameter(torch.ones(1, self.k) / self.k, requires_grad=True)
+        self.q_net = [nn.Linear(d, hidden_dims), nn.ELU()]
+        for i in range(num_layers):
+            self.q_net += [nn.Linear(hidden_dims, hidden_dims), nn.ELU()]
+        self.q_net += [nn.Linear(hidden_dims, k)]
+        self.q_net = nn.Sequential(*self.q_net)
+        self.r_net = [nn.Linear(d+k, hidden_dims), nn.ELU()]
+        for i in range(num_layers):
+            self.r_net += [nn.Linear(hidden_dims, hidden_dims), nn.ELU()]
+        self.r_net += [nn.Linear(hidden_dims, d)]
+        self.r_net = nn.Sequential(*self.r_net)
+        self.beta_prior = nn.Parameter(0.01 * torch.randn(k, d), requires_grad=True)
+    
+    def categorical_kl_divergence(self, p_logits, q_logits):
+        p_softmax = torch.softmax(p_logits, -1)
+        kl = torch.sum(p_softmax * F.log_softmax(p_logits, dim = 1) - p_softmax * F.log_softmax(q_logits, dim = 1), -1)
+        return kl
+    
+    def bernoulli_kl_divergence(self, p_prob, q_prob, eps=1e-4):
+        # q_prob has shape [B, k ,d]
+        # p_prob has shape [B, k, d]
+        kl = p_prob * (torch.log(eps + p_prob) - torch.log(eps + q_prob)) + (1 - p_prob) * (torch.log(eps + 1 - p_prob) - torch.log(eps + 1 - q_prob))
+        return torch.sum(kl, -1)
+    
+    def bernoulli_sample(self, p, T, eps=1e-4):
+        noise = torch.rand_like(p)
+        return torch.sigmoid((torch.log(eps + noise) - torch.log(eps + 1 - noise) + torch.log(eps + p) - torch.log(eps + 1 - p)) / T)
+
+    def elbo(self, inputs, T): # inputs [B, d]
+        z_logits = self.q_net(inputs) # [B, k]
+        z_prob = torch.softmax(z_logits, -1) # [B, K]
+        pred = torch.argmax(z_prob, -1)
+        alpha_posterior_logits = self.r_net(torch.cat([inputs, z_logits], -1)) # [B, d]
+        #beta_prior_logits = z_prob.matmul(self.beta_prior) # [B, d]
+        alpha_posterior_prob = torch.sigmoid(alpha_posterior_logits) # [B, d]
+        #alpha_posterior_prob_sample = self.bernoulli_sample(alpha_posterior_prob, T)
+        alpha_posterior_prob_sample = alpha_posterior_prob
+        f_mu = self.f_mu.unsqueeze(0).repeat(inputs.shape[0], 1, 1) # [B, k, d]
+        b_mu = self.b_mu.unsqueeze(0).repeat(inputs.shape[0], 1, 1) # [B, 1, d]
+        f_comp = D.Normal(f_mu, 1.0)
+        b_comp = D.Normal(b_mu, 1.0)
+        inputs_tiled = inputs.unsqueeze(1).repeat(1, self.k, 1) # [B, k, d]
+        logp_f = f_comp.log_prob(inputs_tiled) #[B, k, d]
+        logp_b = b_comp.log_prob(inputs.unsqueeze(1)) #[B, 1, d]
+        E_log_p_x_given_alpha_z = torch.sum(logp_f * alpha_posterior_prob_sample.unsqueeze(1), -1) + \
+                                  torch.sum(logp_b * (1 - alpha_posterior_prob_sample.unsqueeze(1)), -1) # [B, k]
+        p_beta = torch.sigmoid(5 * self.beta_prior.unsqueeze(0))
+        kl_alpha_beta = self.bernoulli_kl_divergence(alpha_posterior_prob.unsqueeze(1), p_beta) # [B, k]
+        kl_alpha_beta_z = z_prob * kl_alpha_beta
+        E_z = torch.sum(z_prob * E_log_p_x_given_alpha_z - kl_alpha_beta_z, -1) # [B]
+        kl_q_pi = self.categorical_kl_divergence(z_logits, self.prior_pi) # [b]
+        D_beta_prior = D.Normal(torch.sigmoid(5 * self.beta_prior), 0.1*torch.ones(self.k,self.d).to(inputs.device))
+        #D_PI = D.Normal(torch.zeros(self.k, self.d).to(inputs.device), 0.1*torch.ones(self.k,self.d).to(inputs.device))
+        D_PI = D.Uniform(0, 1)
+        kl_Q_PI = torch.mean(D.kl.kl_divergence(D_beta_prior, D_PI))
+        kl_Q_PI = 0
+        return torch.mean(E_z - kl_q_pi) - kl_Q_PI, torch.mean(E_z), torch.mean(kl_alpha_beta_z), torch.mean(kl_q_pi), kl_Q_PI, pred, alpha_posterior_prob, alpha_posterior_prob_sample, z_prob
+    
+    def forward(self, inputs, T):
+        return self.elbo(inputs.float(), T)
+
 
 class GMM(nn.Module):
     ''' A GMM model. '''
     def __init__(
-            self, k, d, tau):
+            self, k, d, tau, mu=None):
         super().__init__()
         self.k = k
         self.d = d
         self.A = nn.Parameter(0.01 * torch.randn(self.k, self.d, self.d), requires_grad=True)
         self.D = nn.Parameter(torch.ones(self.k, self.d), requires_grad=True)
-        self.mu = nn.Parameter(torch.zeros(self.k, self.d), requires_grad=True)
+        if mu is None:
+            self.mu = nn.Parameter(torch.zeros(self.k, self.d), requires_grad=True)
+        else:
+            self.mu = nn.Parameter(torch.Tensor(mu), requires_grad=True)
         self.pi = nn.Parameter(0.01 * torch.randn(self.k), requires_grad=True)
         self.tau = tau
     def negative_log_prob(self, x):
-        sigma = torch.diag_embed(torch.nn.functional.softplus(self.D)) + torch.matmul(self.A, self.A.transpose(1,2))
+        sigma = torch.diag_embed(torch.nn.functional.softplus(self.D)) #+ torch.matmul(self.A, self.A.transpose(1,2))
         sigma = sigma.unsqueeze(0).repeat(x.shape[0], 1, 1, 1) # [bs, k, d, d]
         mu = self.mu.unsqueeze(0).repeat(x.shape[0], 1, 1) # [bs, k, d]
         comp = D.MultivariateNormal(mu, sigma)
@@ -41,21 +245,22 @@ class GMM(nn.Module):
         N, d = samples.shape
         K = self.k
         # Select random d_used coordinates out of d
-        d_used = min(d, max(500, d//8))
-        d_indices = np.random.choice(d, d_used, replace=False)
-        print('Performing k-means clustering to {} components of {} samples in dimension {}/{} ...'.format(K, N, d_used, d))
+        #d_used = min(d, max(500, d//8))
+        #d_indices = np.random.choice(d, d_used, replace=False)
+        print('Performing k-means clustering to {} components of {} samples  ...'.format(K, N))
         with Timer('K-means'):
-            clusters = KMeans(n_clusters=K, max_iter=300, n_jobs=-1).fit(samples[:, d_indices])
+            clusters = KMeans(n_clusters=K, max_iter=300, n_jobs=-1).fit(samples)
         labels = clusters.labels_
         if get_centers:
             centers = np.zeros([K, d])
             for i in range(K):
                 centers[i, :] = np.mean(samples[labels == i, :], axis=0)
-            self.mu = nn.Parameter(torch.Tensor(centers).to(self.mu.device), requires_grad=True)
+            #self.mu = nn.Parameter(torch.Tensor(centers).to(self.mu.device), requires_grad=True)
+            #pdb.set_trace()
             return labels, centers
         return labels
     def check_assignment(self, x):
-        sigma = torch.diag_embed(torch.nn.functional.softplus(self.D)) + torch.matmul(self.A, self.A.transpose(1,2))
+        sigma = torch.diag_embed(torch.nn.functional.softplus(self.D)) #+ torch.matmul(self.A, self.A.transpose(1,2))
         sigma = sigma.unsqueeze(0).repeat(x.shape[0], 1, 1, 1) # [bs, k, d, d]
         mu = self.mu.unsqueeze(0).repeat(x.shape[0], 1, 1) # [bs, k, d]
         comp = D.MultivariateNormal(mu, sigma)
@@ -72,7 +277,7 @@ class ConditionalGMM(nn.Module):
         self.k = k
         self.d = d
         self.f_A = nn.Parameter(0.01 * torch.randn(self.k, self.d, self.d), requires_grad=True)
-        self.f_D = nn.Parameter(torch.ones(self.k, self.d), requires_grad=True)                 # Why is D needed for creating positive definite matrix?
+        self.f_D = nn.Parameter(torch.ones(self.k, self.d), requires_grad=True)
         self.f_mu = nn.Parameter(torch.zeros(self.k, self.d), requires_grad=True)
         self.b_A = nn.Parameter(0.01 * torch.randn(1, self.d, self.d), requires_grad=True)
         self.b_D = nn.Parameter(torch.ones(1, self.d), requires_grad=True)
@@ -101,10 +306,10 @@ class ConditionalGMM(nn.Module):
         return f_d, b_d
 
     def negative_log_prob(self, x):
-        f_sigma = torch.diag_embed(torch.nn.functional.softplus(self.f_D)) + torch.matmul(self.f_A, self.f_A.transpose(1,2))        # A*A.T (for symmetric) + n*eye (for diagonally dominant) ??f
+        f_sigma = torch.diag_embed(torch.nn.functional.softplus(self.f_D)) #+ torch.matmul(self.f_A, self.f_A.transpose(1,2))
         f_sigma = f_sigma.unsqueeze(0).repeat(x.shape[0], 1, 1, 1) # [bs, k, d, d]
         f_mu = self.f_mu.unsqueeze(0).repeat(x.shape[0], 1, 1) # [bs, k, d]
-        b_sigma = torch.diag_embed(torch.nn.functional.softplus(self.b_D)) + torch.matmul(self.b_A, self.b_A.transpose(1,2))
+        b_sigma = torch.diag_embed(torch.nn.functional.softplus(self.b_D)) #+ torch.matmul(self.b_A, self.b_A.transpose(1,2))
         b_sigma = b_sigma.unsqueeze(0).repeat(x.shape[0], 1, 1, 1) # [bs, 1, d, d]
         b_mu = self.b_mu.unsqueeze(0).repeat(x.shape[0], 1, 1) # [bs, 1, d]
         x = x.unsqueeze(1).repeat(1, self.k, 1) # [bs, k, d]
@@ -143,38 +348,6 @@ class ConditionalGMM(nn.Module):
         x = x.unsqueeze(1).repeat(1, self.k, 1) # [bs, k, d]
         labels = torch.argmax(comp.log_prob(x), 1)
         return labels.detach().cpu().numpy()
-
-
-
-class IndependentGMM(nn.Module):
-    ''' A GMM model. '''
-    def __init__(
-            self, k, d, tau):
-        super().__init__()
-        self.k = k
-        self.d = d
-        self.f_mu = nn.Parameter(torch.zeros(self.k, self.d), requires_grad=True)
-        self.b_mu = nn.Parameter(torch.zeros(1, self.d), requires_grad=True)
-        self.pi = nn.Parameter(0.01 * torch.randn(self.k), requires_grad=True)
-        self.alpha = nn.Parameter(0.01 * torch.randn(self.k, self.d), requires_grad=True)
-        self.tau = tau
-
-    def negative_log_prob(self, x):
-        # sigma = torch.diag_embed(torch.nn.functional.softplus(self.D)) #+ torch.matmul(self.A, self.A.transpose(1,2))
-        # sigma = sigma.unsqueeze(0).repeat(x.shape[0], 1, 1, 1) # [bs, k, d, d]
-        f_mu = self.f_mu.unsqueeze(0).repeat(x.shape[0], 1, 1)  # [bs, k, d]
-        b_mu = self.b_mu.unsqueeze(0).repeat(x.shape[0], 1, 1)  # [bs, 1, d]
-        f_comp = D.Normal(f_mu, 1.0)
-        b_comp = D.Normal(b_mu, 1.0)
-        x = x.unsqueeze(1).repeat(1, self.k, 1)  # [bs, k, d]
-        alpha = torch.sigmoid(10 * self.alpha)
-        mix_p = torch.log(torch.softmax(self.tau * self.pi, 0).unsqueeze(0)) + \
-                torch.sum(alpha.unsqueeze(0) * f_comp.log_prob(x), -1) + \
-                torch.sum((1 - alpha.unsqueeze(0)) * b_comp.log_prob(x), -1)
-        assignment = torch.argmax(mix_p, 1)
-        nll = - torch.logsumexp(mix_p, dim=1)
-        return nll.mean(), assignment
-
 
 if __name__ == "__main__":
     model = GMM(10, 64)
---
+++ scripts/train_vgmm.py
@@ -0,0 +1,202 @@
+import os
+import sys
+import pdb
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+p = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+sys.path.append(p)
+import argparse
+import logging
+import torch
+import numpy as np
+from pprint import pformat, pprint
+import sklearn
+import random
+from scipy.stats import norm
+from torch.utils.tensorboard import SummaryWriter
+from hparams import HParams
+#from utils.train_utils import run_epoch, get_gap_lr_bs
+from PIL import Image
+from tqdm import tqdm
+#from torch.utils.tensorboard import SummaryWriter
+
+from scipy.optimize import linear_sum_assignment
+import torch.nn as nn
+import torch.optim as optim
+
+from model import GMM, ConditionalGMM, FB_GMM, Variational_GMM
+from PIL import Image
+
+def soft_thresholding_operator(x, l):
+    idx = torch.where(x>l)
+    x[idx] = x[idx] - l
+    idx = torch.where(x<-l)
+    x[idx] = x[idx] + l
+    idx = torch.where((x>=-l).float() * (x<=l).float())
+    x[idx] = 0
+    return torch.clip(x, 0, 1)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--cfg_file', type=str)
+args = parser.parse_args()
+params = HParams(args.cfg_file)
+pprint(params.dict)
+np.random.seed(params.seed)
+torch.manual_seed(params.seed)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+torch.autograd.set_detect_anomaly(True)
+
+# creat exp dir
+if not os.path.exists(params.exp_dir):
+    os.mkdir(params.exp_dir)
+if not os.path.exists(os.path.join(params.exp_dir, 'gen')):
+    os.mkdir(os.path.join(params.exp_dir, 'gen'))
+if not os.path.exists(os.path.join(params.exp_dir, 'ckpt')):
+    os.mkdir(os.path.join(params.exp_dir, 'ckpt'))
+
+writer = SummaryWriter(params.exp_dir)
+#train_data = np.load('/playpen1/scribble/ssy/data/Levine_32_matrix_train.npy')
+#train_data = train_data[:, :-1]
+#inds = np.random.permutation(train_data.shape[0])
+#train_data = torch.Tensor(train_data[inds]).to(device)
+
+#test_data = np.load('/playpen1/scribble/ssy/data/Levine_32_matrix_test.npy')
+#test_label = test_data[:, -1] - 1
+#test_data = test_data[:, :-1]
+#test_data = torch.Tensor(test_data).to(device)
+orig_test_data = np.load('/playpen1/scribble/ssy/data/Levine_32_matrix_test.npy')
+orig_test_label = orig_test_data[:, -1] - 1
+orig_test_data = orig_test_data[:, :-1]
+inds = np.random.permutation(orig_test_data.shape[0])
+orig_test_data = orig_test_data[inds]
+orig_test_label = orig_test_label[inds]
+train_data = torch.tensor(orig_test_data[:int(0.8*orig_test_data.shape[0])]).to(device)
+test_label = orig_test_label[int(0.8*orig_test_data.shape[0]):]
+test_data = torch.tensor(orig_test_data[int(0.8*orig_test_data.shape[0]):]).to(device)
+#train_data = test_data
+#model = ConditionalGMM(params.k, train_data.shape[1], params.tau).to(device)
+#centers = np.load('./centers.npy')
+#model = GMM(params.k, train_data.shape[1], params.tau).to(device)
+model = Variational_GMM(params.k, train_data.shape[1]).to(device)
+
+
+optimizer = optim.Adam(model.parameters(), lr=params.lr, weight_decay=params.weight_decay)
+best_loss = 1e7
+
+
+
+#labels, clusters = model.kmeans_clustering(train_data.cpu().numpy())
+#beta_dist = torch.distributions.beta.Beta(torch.tensor([0.5]).to(device), torch.tensor([0.5]).to(device))
+
+#mode_0_std = np.linspace(0.2, 0.02, 20)
+#mode_1_std = np.linspace(0.5, 0.1, 20)
+#threshold = 3
+start_epoch = 0
+if params.resume_from_epoch > 0:
+    start_epoch = params.resume_from_epoch
+    checkpoint = torch.load(os.path.join(params.exp_dir, 'ckpt', "epoch_%d.pt" % params.resume_from_epoch))
+    model.load_state_dict(checkpoint)
+T = np.concatenate([np.linspace(5, 0.1, 200), 0.1 * np.ones(params.epochs - 200)])
+for e in range(start_epoch, params.epochs):
+    model.train()
+    epoch_losses = []
+    inds = np.random.permutation(train_data.shape[0])
+    train_data = train_data[inds]
+    if e < 75:
+        lam = 0
+    elif e < 150:
+        lam = 0.1 * params.lam
+    else:
+        lam = params.lam
+
+    #model.mu.requires_grad=False
+    #model.pi.requires_grad=True
+    model.D.requires_grad=False
+    #model.b_D.requires_grad=False
+    #normal_dist_0 = TruncatedNormal(torch.tensor([0.0]).to(device), torch.tensor([mode_0_std[e]]).to(device), a=torch.tensor([0.0]).to(device), b=torch.tensor([1.0]).to(device))
+    #normal_dist_1 = TruncatedNormal(torch.tensor([1.0]).to(device), torch.tensor([mode_1_std[e]]).to(device), a=torch.tensor([0.0]).to(device), b=torch.tensor([1.0]).to(device))
+    for i in tqdm(range(0, train_data.shape[0], params.batch_size)):
+        batch_data = train_data[i:i+params.batch_size]
+        elbo, log_p_x_given_alpha_z, kl_alpha_beta, kl_q_pi, kl_Q_PI, pred, alpha_posterior_prob, alpha_posterior_prob_sample, z_prob = model(batch_data, T[e])
+        beta_prior_prob = torch.sigmoid(5 * model.beta_prior)
+        #weight = torch.norm(beta_prior_prob, p=1, dim=1).unsqueeze(-1) 
+        #weight = weight/weight.max()
+        l1_loss = torch.norm(beta_prior_prob, p=1)
+        total_loss = - elbo + lam * l1_loss
+        #total_loss = nll + lam * beta_loss
+        #print(model.mu)
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
+        epoch_losses.append(total_loss.detach().cpu().item())
+        #print(model.alpha)
+        #
+    train_epoch_loss = np.mean(epoch_losses)
+    writer.add_scalar("log_p_x_given_alpha_z", log_p_x_given_alpha_z, e)
+    writer.add_scalar("kl_alpha_beta", kl_alpha_beta, e)
+    writer.add_scalar("kl_q_pi", kl_q_pi, e)
+    writer.add_scalar("kl_Q_PI", kl_Q_PI, e)
+    writer.add_image("alpha_posterior_prob", alpha_posterior_prob[None, :, :], e)
+    writer.add_image("alpha_posterior_prob_sample", alpha_posterior_prob_sample[None, :, :], e)
+    writer.add_image("beta_prior_prob", torch.sigmoid(5 * model.beta_prior[None, :, :]), e)
+    writer.add_image("z_prior", model.prior_pi[None, :, :], e)
+    writer.add_image("z_prob", z_prob[None, :, :], e)
+    
+    writer.flush()
+    model.eval()
+    epoch_losses = []
+    hist = dict()
+    pred_labels = []
+    v_scores = []
+    for n in tqdm(range(0, test_data.shape[0], params.batch_size)):
+        batch_data = test_data[n:n+params.batch_size]
+        batch_label = test_label[n:n+params.batch_size]
+        elbo, log_p_x_given_alpha_z, kl_alpha_beta, kl_q_pi, kl_Q_PI, pred, alpha_posterior_prob, alpha_posterior_prob_sample, z_prob = model(batch_data, T[e])
+        total_loss = - elbo
+        total_loss.backward()
+        pred = pred.detach().cpu().numpy()
+        v_score = sklearn.metrics.v_measure_score(pred, batch_label)
+        epoch_losses.append(total_loss.detach().cpu().item())
+        v_scores.append(v_score)
+    writer.add_scalar("v_scores", np.mean(v_scores), e)
+    test_epoch_loss = np.mean(epoch_losses)
+
+    
+    
+    
+    #print(hist)
+    print(f"[{e}]/[{params.epochs}], Train Loss: {train_epoch_loss:.4f}, Test Loss: {test_epoch_loss:.4f}, V-scoreL {np.mean(v_scores):.4f}")
+    #print(f"[{e}]/[{params.epochs}], Train Loss: {train_epoch_loss:.4f}, Test Loss: {test_epoch_loss:.4f}")
+    if test_epoch_loss < best_loss:
+        best_test_loss = test_epoch_loss
+        torch.save(model.state_dict(), params.exp_dir + '/ckpt/epoch_%d.pt' % e)
+    if e % 100 == 0:
+        fg_idx = {
+            0 : [13,29], # 0, 5, 11, 12, 13, 17, 19, 23, 24, 25, 28, 29
+            1: [19], #
+            2: [19, 21], # 19, 21, 22, 29
+            3: [7, 23],
+            4: [7, 23, 13],
+            5: [7],
+            6: [25, 5], # 0,  5, 11, 12, 13, 17, 19, 23, 24, 25, 28, 29
+            7: [25, 6], # 6, 11, 12, 13, 19, 23, 24, 25, 28, 29
+            8: [2],
+            9: [15],
+            10: [13, 29], # 0,  5, 11, 12, 13, 17, 19, 23, 24, 25, 28, 29,
+            11: [2, 23], # 2, 10, 12, 17, 22, 23,
+            12: [2, 9], # 0,  2,  3,  9, 14, 23, 24, 29
+            13: [2]
+        }
+        
+        learned_mask = beta_prior_prob.detach().cpu().numpy()
+        learned_mask = (learned_mask > 0.5).astype(np.int32)
+        gt_mask = np.zeros_like(learned_mask, dtype=np.int32)
+        for ii in range(14):
+            for jj in range(len(fg_idx[ii])):
+                gt_mask[ii, fg_idx[ii][jj]] = 1.0
+        dist = np.abs(learned_mask[:, None, :] - gt_mask[None, :, :]).sum(-1)
+        row_ind, col_ind = linear_sum_assignment(dist)
+        for ii in range(14):
+            print(f'learned cluster {ii} has foreground features: {np.where(learned_mask[ii]==1)} and matched to ground-truth foreground annotations: {fg_idx[col_ind[ii]]}.')
